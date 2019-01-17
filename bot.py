c5ab58c5bca7840a783b084a5735ef65a5c01fac
masternovab <46070544+masternovab@users.noreply.github.com>
2019-01-17 18:03:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2210,44 +2210,6 @@ async def movie(ctx, *, name:str=None):
             embed.add_field(name = "__Type__", value = x["Type"])
             embed.set_footer(text = "Information from the OMDB API")
             await client.say(embed=embed)
-            
-@client.event            
-async def on_command_error(self, ctx, error, *args):
-        """The event triggered when an error is raised while invoking a command.
-        ctx   : Context
-        error : Exception"""
-
-        # This prevents any commands with local handlers being handled here in on_command_error.
-        if hasattr(ctx.command, 'on_error'):
-            return
-        
-        ignored = (commands.CommandNotFound, commands.UserInputError)
-        
-        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
-        # If nothing is found. We keep the exception passed to on_command_error.
-        error = getattr(error, 'original', error)
-        
-        # Anything in ignored will return and prevent anything happening.
-        if isinstance(error, ignored):
-            return
-
-        elif isinstance(error, commands.DisabledCommand):
-            return await ctx.send(f'{ctx.command} has been disabled.')
-
-        elif isinstance(error, commands.NoPrivateMessage):
-            try:
-                return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
-            except:
-                pass
-
-        # For this error example we check to see where it came from...
-        elif isinstance(error, commands.BadArgument):
-            if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
-                return await ctx.send('I could not find that member. Please try again.')
-
-        # All other Errors not returned come here... And we can just print the default TraceBack.
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 @client.command()
 async def fancify(*, text):
